8e00fff59c3b906d010570b075225a1658b904fa
mikeyrayvon <mikey@interglobal.vision>
2018-03-01 11:09:59 -0600

    change scrollto offset

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ Site = {
 
     $('.js-scroll-to-downloads').click(function() {
       $('html, body').animate({
-          scrollTop: $("#downloads").offset().top
+          scrollTop: $("#downloads").offset().top - 30
       }, 1500);
     });
 
